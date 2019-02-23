3e6b8fd9b6c9857331bf15c5957e901c423a688b
Lucas Nunes <nunesmlucasg@gmail.com>
2019-02-23 13:27:32 -0500

    almost done with second challenge

--- bamazonManager.js
+++ bamazonManager.js
@@ -109,107 +109,56 @@ function viewLowInventory() {
 };
 
 function addToInventory() {
-    inquirer
-        .prompt([
-            {
-                name: "choice",
-                type: "input",
-                message: "What is the item (use ID) you would like to restock?"
-            },
-            {
-                name: "numberToAdd",
-                type: "input",
-                message: "How many would you like to re-order?"
-            },
-        ])
-        .then(function (answer) {
-            // when finished prompting, insert a new item into the db with that info
-            connection.query("SELECT * FROM products WHERE item_id = " + answer.choice),
-                function (err) {
-                    if (err) throw err;
-
-                    // UPDATING HERE ------- 
-                    connection.query("UPDATE  FROM products WHERE item_id = " + answer.choice),
-
-                }
-
-            // connection.query(
-            //     "UPDATE products SET ? WHERE ?", [
-            //         {
-            //             stock_quantity: answer.numberToAdd
-            //         },
-            //         {
-            //             item_id: answer.item_id
-            //         }]
-            //     function (err) {
-            //         if (err) throw err;
-            //         console.log("Your auction was created successfully!");
-            //         // re-prompt the user for if they want to bid or post
-            //         start();
-            //     }
-            // );
+    connection.query("SELECT * FROM products", function (err, results) {
+        var table = new Table({
+            head: ['ID', 'Product', 'Department', 'Price', 'Stock #'],
+            colWidths: [5, 20, 20, 10, 10]
         });
-}
+        for (var i = 0; i < results.length; i++) {
+            table.push(
+                [results[i].item_id,
+                results[i].product_name,
+                results[i].department_name,
+                "$" + (results[i].price),
+                results[i].stock_quantity]
+            )
+        }
+        console.log(table.toString());
 
-// function bidAuction() {
-//   // query the database for all items being auctioned
-//   connection.query("SELECT * FROM auctions", function(err, results) {
-//     if (err) throw err;
-//     // once you have the items, prompt the user for which they'd like to bid on
-//     inquirer
-//       .prompt([
-//         {
-//           name: "choice",
-//           type: "rawlist",
-//           choices: function() {
-//             var choiceArray = [];
-//             for (var i = 0; i < results.length; i++) {
-//               choiceArray.push(results[i].item_name);
-//             }
-//             return choiceArray;
-//           },
-//           message: "What auction would you like to place a bid in?"
-//         },
-//         {
-//           name: "bid",
-//           type: "input",
-//           message: "How much would you like to bid?"
-//         }
-//       ])
-//       .then(function(answer) {
-//         // get the information of the chosen item
-//         var chosenItem;
-//         for (var i = 0; i < results.length; i++) {
-//           if (results[i].item_name === answer.choice) {
-//             chosenItem = results[i];
-//           }
-//         }
+        inquirer
+            .prompt([
+                {
+                    name: "choice",
+                    type: "input",
+                    message: "What is the item (use ID) you would like to restock?"
+                },
+                {
+                    name: "numberToAdd",
+                    type: "input",
+                    message: "How many would you like to re-order?"
+                },
+            ])
+            .then(function (answer) {
+                // when finished prompting, insert a new item into the db with that info
+                // console.log(answer);
+                for (var i = 0; i < results.length; i++) {
+                    if (results[i].item_id == parseInt(answer.choice)) {
+                        connection.query("UPDATE products SET ? WHERE ?",
+                            [{
+                                stock_quantity: results[i].stock_quantity + parseInt(answer.numberToAdd)
+                            },
+                            {
+                                item_id: answer.choice
+                            }],
+                            function (err) {
+                                if (err) throw err;
+                                // console.log(answer.choice);
+                            }
+                        );
+                    }
+                }
+                start();
+            });
 
-//         // determine if bid was high enough
-//         if (chosenItem.highest_bid < parseInt(answer.bid)) {
-//           // bid was high enough, so update db, let the user know, and start over
-//           connection.query(
-//             "UPDATE auctions SET ? WHERE ?",
-//             [
-//               {
-//                 highest_bid: answer.bid
-//               },
-//               {
-//                 id: chosenItem.id
-//               }
-//             ],
-//             function(error) {
-//               if (error) throw err;
-//               console.log("Bid placed successfully!");
-//               start();
-//             }
-//           );
-//         }
-//         else {
-//           // bid wasn't high enough, so apologize and start over
-//           console.log("Your bid was too low. Try again...");
-//           start();
-//         }
-//       });
-//   });
-// }
+    });
+};
